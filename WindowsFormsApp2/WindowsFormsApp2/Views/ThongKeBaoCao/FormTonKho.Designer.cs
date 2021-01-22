fc92752cf526fa09f4d0edbf2783915286ae41f8
Nguyen Hoang Anh <hoanganh25022000@gmail.com>
2021-01-22 22:08:50 +0700

    Tồn Kho

---
+++ WindowsFormsApp2/WindowsFormsApp2/Views/ThongKeBaoCao/FormTonKho.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApp2.Views.ThongKeBaoCao
+{
+    partial class FormTonKho
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.TonKhoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.TonKhoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            reportDataSource1.Name = "TonKhoDataSet";
+            reportDataSource1.Value = this.TonKhoBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WindowsFormsApp2.Views.ThongKeBaoCao.TonKho.TonKhoReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(25, 38);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(763, 400);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // TonKhoBindingSource
+            // 
+
+            // 
+            // FormTonKho
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "FormTonKho";
+            this.Text = "FormTonKho";
+            this.Load += new System.EventHandler(this.FormTonKho_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TonKhoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource TonKhoBindingSource;
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Views/ThongKeBaoCao/FormTonKho.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp2.Models;
+using WindowsFormsApp2.Views.ThongKeBaoCao.TonKho;
+
+namespace WindowsFormsApp2.Views.ThongKeBaoCao
+{
+    public partial class FormTonKho : Form
+    {
+        Model1 context = new Model1();
+        public FormTonKho()
+        {
+            InitializeComponent();
+        }
+
+        private void FormTonKho_Load(object sender, EventArgs e)
+        {
+            List<SanPhamTonKho> listReport = new List<SanPhamTonKho>();
+            List<Product> list = context.Products.ToList();
+            foreach (Product i in list)
+            {
+                SanPhamTonKho r = new SanPhamTonKho();
+                r.id = i.id;
+                r.Name = i.Name;
+                r.ProductDate = i.ProductDate.Value;
+                r.Quantity = i.Quantity.Value;
+                r.UnitPrice = i.UnitPrice;
+
+                listReport.Add(r);
+            }
+            this.reportViewer1.LocalReport.ReportPath = "TonKhoReport.rdlc";
+            var resource = new ReportDataSource("TonKhoDataSet", listReport);
+            this.reportViewer1.LocalReport.DataSources.Clear();
+            this.reportViewer1.LocalReport.DataSources.Add(resource);
+            this.reportViewer1.RefreshReport();
+
+        }
+    }
+}
